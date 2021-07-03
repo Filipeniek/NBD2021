225c294db3548587819dda0f6ff4fd854edba94b
Filipeniek <86184389+Filipeniek@users.noreply.github.com>
2021-07-03 10:52:54 +0200

    Delete zapytanie_9.js

--- zapytanie_9.js
+++
@@ -1,2 +0,0 @@
-printjson(db.people.updateMany({first_name:"Antonio"}, {$set:{hobby: "pingpong"}}));
-printjson(db.people.find({first_name:"Antonio"},{first_name:1, last_name:1, hobby:1, _id:0}).toArray());
